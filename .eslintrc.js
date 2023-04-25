81a876b88202b612c09f0d5b835aed960e231a52
Marcelo Junior <marcelo_vicentejr@yahoo.com.br>
2023-04-25 20:30:29 -0300

    Primeiro Commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    ecmaVersion: 'latest',
+    requireConfigFile: false,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': 0,
+  },
+};
---
+++ src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>
+        Login
+        <small>Oii</small>
+      </Title>
+      <p>Lorem.</p>
+    </Container>
+  );
+}
