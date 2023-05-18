04c87c4e075036edf931aacf4f41e9b3e757f985
Marcelo Junior <marcelo_vicentejr@yahoo.com.br>
2023-05-18 18:58:00 -0300

    Redux

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './styled';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: 'BOTAO_CLICADO',
+    });
+  }
+
   return (
     <Container>
       <Title>
@@ -11,7 +22,9 @@ export default function Login() {
         <small>Oii</small>
       </Title>
       <p>Lorem.</p>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
