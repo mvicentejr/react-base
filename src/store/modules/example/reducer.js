6bf30d9728dfc38d442f52c8130be01953809e59
Marcelo Junior <marcelo_vicentejr@yahoo.com.br>
2023-05-30 20:09:25 -0300

    Redux Saga

--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -1,3 +1,5 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
@@ -5,10 +7,24 @@ const initialState = {
 // eslint-disable-next-line default-param-last, func-names
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCCESS: {
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
+      // eslint-disable-next-line no-console
+      console.log('Botão Clicado.');
       return newState;
+    }
+
+    case types.BOTAO_CLICADO_FAILURE: {
+      // eslint-disable-next-line no-console
+      console.log('Deu erro.');
+      return state;
+    }
+
+    case types.BOTAO_CLICADO_REQUEST: {
+      // eslint-disable-next-line no-console
+      console.log('Estou fazendo a requisição.');
+      return state;
     }
 
     default:
